84f147b3829238c2c6c3dfb2f100c49c89af1517
Cuong Ngo <dinhcuongngo@gmail.com>
2025-06-03 16:25:54 +0700

    feat: seperate Navigation Drawer component

--- utils/index.ts
+++ utils/index.ts
@@ -5,3 +5,7 @@
 export const publicAssetPath = (path: string) => {
   return `${useRuntimeConfig().app.baseURL}${path}`;
 }
+
+export const dashboardImgPath = (img: string) => {
+return `${useRuntimeConfig().app.baseURL}/images/dashboard/${img}`;
+}
