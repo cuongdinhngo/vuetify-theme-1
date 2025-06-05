ccbe08c08e3379c6ffb535a134aabd45ef0a2050
Cuong Ngo <dinhcuongngo@gmail.com>
2025-06-05 22:18:11 +0700

    feat: use localStorage to store current theme

--- components/dashboards/header/ToggleTheme.vue
+++ components/dashboards/header/ToggleTheme.vue
@@ -5,12 +5,19 @@
 </template>
 <script setup lang="ts">
 import { useTheme } from 'vuetify';
+import { useStorage } from '@vueuse/core'
 
 const theme = useTheme();
 
-const isDark = computed(() => theme.global.name.value === 'dark');
+const isDark = computed(() => {
+  const currentTheme = useStorage('vuetify-theme', 'light');
+  theme.global.name.value = currentTheme.value;
+  console.log('Current theme:', currentTheme.value, theme.global.name.value);
+  return currentTheme.value === 'dark'
+});
 
 const toggleTheme = () => {
   theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark';
+  localStorage.setItem('vuetify-theme', theme.global.name.value);
 };
 </script>
