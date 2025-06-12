086610a6d11ed963da2defb061dcd00422fafc4e
Cuong Ngo <dinhcuongngo@gmail.com>
2025-06-12 12:14:33 +0700

    feat: DataTable Top includes left search field and right create button

---
+++ components/dashboards/section/DataTableTop.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="d-flex justify-space-between align-center">
+    <slot name="left"></slot>
+
+    <v-spacer></v-spacer>
+
+    <slot name="right"></slot>
+  </div>
+</template>
