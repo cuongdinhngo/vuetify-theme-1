7d322b1c0e9bb43e94956feab586a26f7b34f9d8
Cuong Ngo <dinhcuongngo@gmail.com>
2025-06-13 22:55:07 +0700

    feat: ecom index page

---
+++ components/ecom/drawer/PriceRange.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-expansion-panels variant="accordion" elevation="0" multiple v-model="panel">
+    <v-expansion-panel>
+      <v-expansion-panel-title class="text-subtitle-1 font-weight-medium ma-0">
+        Price Range
+      </v-expansion-panel-title>
+      <v-expansion-panel-text>
+        <v-range-slider
+          v-model="priceRange"
+          thumb-label="always"
+          step="1"
+          min="0"
+          max="500"
+          color="primary"
+        ></v-range-slider>
+      </v-expansion-panel-text>  
+    </v-expansion-panel>
+    <v-divider></v-divider>
+  </v-expansion-panels>
+</template>
+<script setup lang="ts">
+const panel = ref<number[]>([0]);
+
+const priceRange = defineModel('priceRange', {
+  type: Array<number>,
+  default: () => [0, 500],
+});
+
+watch(priceRange, (newValue) => {
+  console.log('Price range updated:', newValue);
+});
+</script>
---
+++ components/ecom/header/Mobile.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app-bar
+    color="primary"
+    :elevation="4"
+  >
+    <div class="d-flex align-center ml-2">
+      <v-icon class="mr-1">mdi-shopping-outline</v-icon>
+      <v-img
+        :src="dashboardImgPath('mobile_logo.png')"
+        width="30"
+        height="30"
+      ></v-img>
+    </div>
+    <v-spacer></v-spacer>
+    <div class="d-flex ga-2 align-center mr-2">
+      <v-btn variant="tonal" icon size="small">
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+      <v-btn variant="tonal" icon size="small">
+        <v-icon>mdi-basket-outline</v-icon>
+      </v-btn>
+      <v-btn variant="tonal" icon size="small">
+        <v-icon>mdi-account</v-icon>
+      </v-btn>
+    </div>
+  </v-app-bar>
+</template>
