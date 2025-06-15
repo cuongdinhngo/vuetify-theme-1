f35f2fda1997dec6ee44e2f2720f8520638aab87
Cuong Ngo <dinhcuongngo@gmail.com>
2025-06-15 14:04:58 +0700

    chore: Banner component

--- layouts/ecom.vue
+++ layouts/ecom.vue
@@ -7,26 +7,9 @@
       :class="[!smAndDown ? 'pa-0' : '', 'bg-background']"
     >
       <!-- Banner Section -->
-      <div
+      <ecom-banner
         v-if="!smAndDown"
-        class="banner-section"
-      >
-        <v-img
-          src="https://picsum.photos/1100/300"
-          height="400"
-          cover
-          class="banner-image"
-        >
-          <div class="banner-overlay">
-            <v-container class="d-flex align-center justify-center h-100">
-              <div class="text-center text-white">
-                <h1 class="text-h3 mb-4">Welcome to Our Store</h1>
-                <p class="text-h6">Discover amazing products</p>
-              </div>
-            </v-container>
-          </div>
-        </v-img>
-      </div>
+      />
 
       <!-- Main Content -->
       <v-container>
