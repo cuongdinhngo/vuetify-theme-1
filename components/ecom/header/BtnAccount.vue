5511ba41293101d217b14317ac60e41b3d876cdf
Cuong Ngo <dinhcuongngo@gmail.com>
2025-06-15 14:04:15 +0700

    chore: UX header buttons

--- components/ecom/header/BtnAccount.vue
+++ components/ecom/header/BtnAccount.vue
@@ -2,13 +2,12 @@
   <v-hover v-slot="{ isHovering, props }">
     <v-btn
       variant="tonal"
-      color="primary"
       icon
       size="small"
-      :class="{ 'bg-primary': isHovering }"
+      :class="{'bg-primary': !isHovering}"
       v-bind="props"
     >
-      <v-icon :color="isHovering ? 'white' : 'primary'">mdi-account</v-icon>
+      <v-icon :color="isHovering ? 'primary' : 'white'">mdi-account</v-icon>
     </v-btn>
   </v-hover>
 </template>
--- components/ecom/header/BtnCart.vue
+++ components/ecom/header/BtnCart.vue
@@ -2,13 +2,12 @@
   <v-hover v-slot="{ isHovering, props }">
     <v-btn
       variant="tonal"
-      color="primary"
       icon
       size="small"
-      :class="{ 'bg-primary': isHovering }"
+      :class="{'bg-primary': !isHovering}"
       v-bind="props"
     >
-      <v-icon :color="isHovering ? 'white' : 'primary'">mdi-basket-outline</v-icon>
+      <v-icon :color="isHovering ? 'primary' : 'white'">mdi-basket-outline</v-icon>
     </v-btn>
   </v-hover>
 </template>
